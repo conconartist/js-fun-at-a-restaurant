9babff717263a68588efe9094b8a00e9c5871305
Chung Hong <conconart.info@gmail.com>
2020-10-07 19:53:17 -0600

    Add function refundOrder to order

--- src/order.js
+++ src/order.js
@@ -7,9 +7,17 @@ function takeOrder(order, deliveryOrders) {
   }
 }
 
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if(orderNumber === deliveryOrders[i].orderNumber){
+      deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
 module.exports = {
   takeOrder,
-  // refundOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
