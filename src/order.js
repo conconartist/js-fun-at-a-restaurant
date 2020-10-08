e923569cca3bbc01cab4a5aa0f4c69d26da2131a
Chung Hong <conconart.info@gmail.com>
2020-10-07 19:05:20 -0600

    Add takeOrder function to order

--- src/order.js
+++ src/order.js
@@ -1,6 +1,14 @@
 
+function takeOrder(order, deliveryOrders) {
+  if(deliveryOrders.length < 3) {
+  deliveryOrders.push(order);
+  } else {
+    return deliveryOrders;
+  }
+}
+
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
