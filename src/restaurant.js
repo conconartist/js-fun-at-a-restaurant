19f398f2d0fe425a2030cd625f3d963dca9d4536
Chung Hong <conconart.info@gmail.com>
2020-10-08 10:52:24 -0600

    Add createRestaurant function to restaurant file

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,13 @@
-
+function createRestaurant(name) {
+  var restaurant = {
+    name: name,
+    menus: {breakfast: [], lunch: [], dinner: []}
+  }
+  return restaurant;
+}
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant,
   // addMenuItem,
   // removeMenuItem
 }
